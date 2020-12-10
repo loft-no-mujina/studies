d3945d45560127af090d8b966d8c2f151c02efe0
loft-no-mujina <50648319+loft-no-mujina@users.noreply.github.com>
2020-12-10 23:57:02 +0900

    Create VendingMachineMain.java

---
+++ VendingMachineMain.java
@@ -0,0 +1,12 @@
+class VendingMachineMain{
+
+	class item(String name,int stock,int price){
+		
+	}
+
+	public static void main(String[] args){
+		System.out.println("＝＝＝＝＝自動販売機アプリケーション＝＝＝＝＝");
+		item aaa = new item;
+		System.out.println(args);
+	}
+}
